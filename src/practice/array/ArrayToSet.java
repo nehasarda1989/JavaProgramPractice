baf70aaa5bb54d0c1d80b9bc76c1fbad7498eedb
nehasarda1989 <nehasarda1989@gmail.com>
2022-02-16 17:36:05 +0530

    added new class

---
+++ src/practice/array/ArrayToSet.java
@@ -0,0 +1,15 @@
+package practice.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayToSet {
+	 public static void main(String args[]) {
+	      String[] array = {"a", "b", "c", "d", "e"};
+	      
+	      
+	      
+	 }
+}
--- src/practice/string/AllSubPallindrome.java
+++ src/practice/string/AllSubPallindrome.java
@@ -1,5 +1,11 @@
 package practice.string;
 
 public class AllSubPallindrome {
+	
+	 public static void main(String[] args) {
+	        String s = "abbaeae";
+	  
+	       
+	    }
 
 }
